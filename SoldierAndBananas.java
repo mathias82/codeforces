9dfc2228610b6d3af5c51788f61d6e7618c8e328
STAVROU Matthaios <matthaios.stavrou@intrasoft-intl.com>
2021-09-21 09:19:10 +0300

    A. Soldier and Bananas

---
+++ SoldierAndBananas.java
@@ -0,0 +1,29 @@
+package com.project.controller.codeforces;
+
+import java.util.Scanner;
+
+public class SoldierAndBananas {
+
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        int k,n,w;
+        int sum=0;
+        int borrow=0;
+
+        k = sc.nextInt();
+        n = sc.nextInt();
+        w = sc.nextInt();
+
+        for (int i=1; i<=w; i++){
+
+            sum += i * k;
+        }
+
+        if (sum > n){
+            borrow = sum - n;
+        }
+
+        System.out.println(borrow);
+    }
+}
