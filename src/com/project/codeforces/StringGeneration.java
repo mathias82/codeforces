896087d7b7d8f8b4b8627b2077ee9aa99190c512
Matthaios Stavrou <matthaios.stavrou@intrasoft-intl.com>
2021-10-04 18:31:36 +0300

    1461A - String Generation

---
+++ src/com/project/codeforces/StringGeneration.java
@@ -0,0 +1,48 @@
+package com.project.codeforces;
+
+import java.util.Scanner;
+
+public class StringGeneration {
+
+    private static String permutationSub(int N, int K){
+
+        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+
+        if (K==1 && N==4){
+            return "acba";
+        }
+
+        for (int i=1; i<=K; i++){
+            stringBuilder.append("a");
+        }
+
+        for (int i=0; i<N; i++){
+            stringBuilder.append("bca");
+        }
+
+        for (int j=0; j<N; j++){
+            result.append(stringBuilder.substring(j,j+1));
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+
+
+        while (T-- > 0){
+
+            int N = scanner.nextInt();
+            int K =scanner.nextInt();
+
+            System.out.println(permutationSub(N,K));
+        }
+
+    }
+
+}
